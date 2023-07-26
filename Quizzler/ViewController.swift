0a67b044a95c16c00880ddf768f55ca523e51299
Privetyanikita <72889842+Privetyanikita@users.noreply.github.com>
2023-07-26 20:49:59 +0200

    added the interface, made the constreints

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func ansverQusTupped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        print(buttonTitle)
+    }
+    
 
 }
 
