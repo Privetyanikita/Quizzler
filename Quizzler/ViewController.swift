54df5efb0d8dbe54417f7e1f4e2390338a284a5a
Privetyanikita <72889842+Privetyanikita@users.noreply.github.com>
2023-08-01 23:36:25 +0200

    add new func

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -17,15 +17,61 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var falseButton: UIButton!
     
+    
+    let brainSt = Question(q: <#T##String#>, a: <#T##String#>)
+    
+    var questionNumber = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        appdateUI()
     }
-    @IBAction func ansverQusTupped(_ sender: UIButton) {
+    @IBAction func answerQusTupped(_ sender: UIButton) {
         guard let buttonTitle = sender.titleLabel?.text else { return }
+        switch buttonTitle {
+        case "True":
+            if brainSt.checkAnswer(answer: true, questionNumber: questionNumber) == true{
+                sender.backgroundColor = UIColor.green
+            }
+            else{
+                sender.backgroundColor = UIColor.red
+            }
+        case "False":
+            if brainSt.checkAnswer(answer: false, questionNumber: questionNumber) == false{
+                sender.backgroundColor = UIColor.green
+            }
+            else{
+                sender.backgroundColor = UIColor.red
+            }
+        default:
+            break
+        }
+        if questionNumber == brainSt.quizBrain.count-1{
+            appdateUI(text: "End of the quiz")
+        }
+        else{
+            questionNumber += 1
+        }
+        
         print(buttonTitle)
     }
     
+    private func appdateUI(){
+        questionLabel.text = brainSt.quizBrain[questionNumber]
+        progressView.progress = Float(questionNumber) / Float(brainSt.quizBrain.count)
+    }
+    
+    private func appdateUI(text: String){
+        questionLabel.text = text
+        progressView.progress = Float(questionNumber) / Float(brainSt.quizBrain.count)
+        updateButton()
+    }
+    func updateButton(){
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+
 
 }
 
